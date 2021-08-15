673ab5b5a2a0eae73e8b4df7af950511c5c7db87
Cam Chandler <camchandler15@gmail.com>
2021-08-15 13:17:57 -0400

    made navbar a flexbox, added styling to active class

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-// import './Navigation.css';
+import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -22,12 +22,14 @@ function Navigation({ isLoaded }){
   }
 
   return (
+    <header>
     <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
         {isLoaded && sessionLinks}
       </li>
     </ul>
+    </header>
   );
 }
 
