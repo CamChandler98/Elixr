81215d807d3c0cc6bb53f6cb4cdb6d90d7d6b79c
Cam Chandler <camchandler15@gmail.com>
2021-08-16 13:21:56 -0400

    added thunk to fetch add drink

--- frontend/src/store/drinks.js
+++ frontend/src/store/drinks.js
@@ -1,18 +1,39 @@
-const LOAD = 'drinks/LOAD';
+const LOAD = 'drinks/load';
+const ADD_DRINK = 'drinks/addDrink'
 
 const load = list => ({
     type: LOAD,
     list
 })
 
+
+const addOneDrink = drink =>({
+    type: ADD_DRINK,
+    drink
+})
+
 export const getDrinks = () => async (dispatch) => {
     const res = await fetch('/api/drinks')
     if (res.ok){
         const list = await res.json()
-        console.log('list',list)
         dispatch(load(list))
     }
 }
+
+export const addDrink = (drink) => async (dispatch) => {
+    let res = await fetch ('/api/drinks',{
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(drink)
+    })
+
+    if(res.ok){
+        let newDrink = await res.json()
+        dispatch(addOneDrink(newDrink))
+        return newDrink
+    }
+}
+
 
 
 
@@ -23,10 +44,22 @@ const drinkReducer = (state = {drinks:{}}, action) => {
                 accum[drink.id] = drink
                 return accum
             },{})
-
             return{
                 ...state,
-                drinks: drinks
+                ...drinks
+            }
+        }
+        case ADD_DRINK:{
+            if(!state[action.drink.id]){
+                return{
+                    ...state,
+                    [action.drink.id] :action.drink
+                }
+            }else{
+                // return{
+                //     ...state,
+                //     [action.dr]
+                // }
             }
         }
         default:
