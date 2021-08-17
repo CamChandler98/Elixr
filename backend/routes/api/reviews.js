83b360a76aeb275e0c18c6ff2738487e23b96b3f
Cam Chandler <camchandler15@gmail.com>
2021-08-17 13:22:15 -0400

    made and imported review router

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+const {Review} = require('../../db/models')
+
+const router = express.Router();
+
+router.get('/drinks/:drinkId',)
+
+module.exports = router
