6ee541d7f94108984493ffea0ebfe209a052a6f6
Cam Chandler <camchandler15@gmail.com>
2021-08-17 17:26:50 -0400

    added route to post review

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,11 +1,41 @@
 import { csrfFetch } from './csrf';
 
 const LOAD = 'reviews/load'
+const ADD = 'reviews/add'
+
+const add = review => ({
+    type: ADD,
+    review
+})
 
 const load = reviewList => ({
     type: LOAD,
     reviewList
 })
+
+export const addReview = (review) => async (dispatch) => {
+    const {image, userId , drinkId, rating, content} = review
+
+    const formData = new FormData()
+
+    formData.append('userId',userId)
+    formData.append('drinkId', drinkId)
+    formData.append('rating',rating)
+    formData.append('content',content)
+    if(image) formData.append("image",image)
+    const res = await csrfFetch(`/api/reviews`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: formData,
+      });
+
+      const review = await res.json();
+      dispatch(add(review));
+
+
+}
 
 export const getDrinkReviews = (drinkId) => async (dispatch) =>{
     const res = await fetch(`/api/reviews/drinks/${drinkId}`)
@@ -22,6 +52,7 @@ export const getUserReviews = (userId) => async (dispatch) =>{
         dispatch(load(reviewList))
     }
 }
+
 const reviewReducer = (state = {}, action) => {
     switch(action.type){
         case LOAD:{
@@ -32,6 +63,22 @@ const reviewReducer = (state = {}, action) => {
             return{
                 ...state,
                 ...reviews
+            }
+        }
+        case ADD: {
+            if(!state[action.review.id]){
+                return{
+                    ...state,
+                    [action.review.id] : action.review
+                }
+            }else{
+                return{
+                    ...state,
+                    [action.review.id]: {
+                        ...state[action.review.id],
+                        ...action.review
+                    }
+                }
             }
         }
         default:
