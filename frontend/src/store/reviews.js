21252806770ae50369650afb984b05b29acf5ae1
Cam Chandler <camchandler15@gmail.com>
2021-08-17 16:57:24 -0400

    made review reducer

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,16 @@
+import { csrfFetch } from './csrf';
+
+const LOAD = ''
+
+
+const reviewReducer = (state = {}, action) => {
+    switch(action.type){
+        case LOAD:{
+
+        }
+        default:
+            return state
+    }
+}
+
+export default reviewReducer
