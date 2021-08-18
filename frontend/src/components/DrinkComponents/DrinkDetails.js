c90b8733b7f1fc9a622f568a929783d2d7f3b207
Cam Chandler <camchandler15@gmail.com>
2021-08-17 22:00:10 -0400

    added avg rating tto drink page

--- frontend/src/components/DrinkComponents/DrinkDetails.js
+++ frontend/src/components/DrinkComponents/DrinkDetails.js
@@ -25,13 +25,13 @@ const DrinkDetails = ({drinkId}) =>{
                     <NavLink to = {`/drinks/${drinkId}`}>
                     <h2>{drink?.name}</h2>
                     </NavLink>
-                    <h3>TODO: Add Creator Name</h3>
-                    <h3>TODO: Add Category Name</h3>
+                    <h3>{drink?.User.username}</h3>
+                    <h3>{drink?.Category.name}</h3>
                 </div>
             </div>
             <div className = 'drink-details-right'>
-                <h2 className= 'total-ratings'>TODO: ADD RATING COUNT</h2>
-                <span className = 'average-rating'>TODO ADD AVERAGE</span>
+                <h2 className= 'total-ratings'>Total ratings {drink?.count}</h2>
+                <span className = 'average-rating'>Average rating: {drink?.avg}</span>
             </div>
         </div>
     )
