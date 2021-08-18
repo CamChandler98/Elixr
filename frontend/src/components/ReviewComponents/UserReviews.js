a7c696410405b1e8840b5dcba880ad8a7ccb06a0
Cam Chandler <camchandler15@gmail.com>
2021-08-17 23:32:34 -0400

    added user case to review reducer

---
+++ frontend/src/components/ReviewComponents/UserReviews.js
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux"
+
+const UserReviews = () => {
+    let dispatch = useDispatch()
+
+    
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default UserReviews
