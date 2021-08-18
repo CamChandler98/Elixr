cc786e1e5d1793ffeb2b6aa750f51897f4a7aa1a
Cam Chandler <camchandler15@gmail.com>
2021-08-17 23:36:09 -0400

    added dispatch to get users reviews in userreviews coponent

--- frontend/src/components/ReviewComponents/UserReviews.js
+++ frontend/src/components/ReviewComponents/UserReviews.js
@@ -1,10 +1,16 @@
-import { useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getUserReviews } from "../../store/reviews"
 
 const UserReviews = () => {
     let dispatch = useDispatch()
+    let userId = useSelector(state => state.session.user.id)
+    useEffect(()=>{
+        dispatch(getUserReviews(userId))
+    })
 
-    
-
+    let reviews = useSelector (state => state.reviews.UserReviews )
+    console.log(reviews)
     return(
         <div>
 
