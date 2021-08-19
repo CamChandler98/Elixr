540a5fccb7b46dd2035b95f437c1536ef29023b2
Cam Chandler <camchandler15@gmail.com>
2021-08-19 16:38:21 -0400

    added slider styling

--- frontend/src/components/ReviewComponents/AddReviewForm.js
+++ frontend/src/components/ReviewComponents/AddReviewForm.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import ReactSlider from "react-slider"
 import { useDispatch, useSelector } from "react-redux"
 import { addReview } from "../../store/reviews"
 import styled from "styled-components"
@@ -6,6 +7,9 @@ import { getOneDrink } from "../../store/drinks"
 import cameraButton from '../DrinkComponents/images/thumbnail/photo-button.svg'
 
 const AddReviewSty = styled.div`
+h2{
+    color: rgb(49 45 45 / 69%)
+}
 form{
     display: flex;
     flex-direction: column;
@@ -37,12 +41,73 @@ img{
     height: 30px;
     border-radius: 8px;
     border: 0px;
-    background-color: rgb(143,69,182);
+    background-color:  rgb(198, 135, 231);
     color: #fff;
     font-weight: 500;
 }
+.rating-container{
+    display:flex;
+    gap:10px;
+}
+
+.rating-container label{
+    font-weight: bold;
+    align-self: center;
+    font-size: 20px;
+    color: rgb(49 45 45 / 69%);
+}
+textarea{
+    resize:none;
+    border-color:  rgba(128, 128, 128, 1);
+    background: transparent;
+    padding: 5%;
+    font-size: 16px;
+    box-sizing: border-box;
+    outline: none;
+}
 `
 
+const ReviewSliderSty = styled.div`
+
+  .rating-thumb {
+    height: 30px;
+    line-height: 30px;
+    width: 30px;
+    text-align: center;
+    background-color:rgb(63 51 70);
+    color: #fff;
+    border-radius: 50%;
+    top: -2px;
+    cursor: grab;
+  }
+  .rating-slider {
+    width: 200px;
+    height: 5px;
+  }
+
+
+  .rating-track {
+    background: ${props => (props.index === 2 ? '#f00' : props.index === 1 ? '#0f0' : '#ddd')};
+    border-radius: 999px;
+    height:30px;
+    top: -2px;
+    bottom: 0;
+  }
+
+  .rating-track.rating-track-0 {
+    background: rgb(198, 135, 231);
+  }
+
+  .slider-container{
+    resize: horizontal;
+
+    width: 500px
+    max-width: 100%;
+    padding-right: 8px;
+  }
+
+
+`
 const AddReviewForm = ({drinkId}) =>{
     const dispatch = useDispatch()
 
@@ -104,11 +169,11 @@ const AddReviewForm = ({drinkId}) =>{
                        }}>remove</button>}
                     </label>
                     </div>
-                <div>
+                <div className ='rating-container'>
                     <label htmlFor = "rating">
-                        ADD SLIDDER
+                       Was it Magical?
                     </label>
-                    <select
+                    {/* <select
                         value = {rating}
                         onChange = { e => setRating(e.target.value)}
                     >
@@ -117,9 +182,27 @@ const AddReviewForm = ({drinkId}) =>{
                         <option value = {3}>3</option>
                         <option value = {4}>4</option>
                         <option value = {5}>5</option>
-                    </select>
+                    </select> */}
+                    <ReviewSliderSty>
+                        <div className = 'slider-container'>
+                    <ReactSlider
+                        className="rating-slider"
+                        marks
+                        markClassName="rating-mark"
+                        min={1}
+                        max={5}
+                        thumbClassName="rating-thumb"
+                        trackClassName="rating-track"
+                        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+                        renderTrack={(props, state) => (
+                            <div {...props} index={state.index}></div>
+                          )}
+                          pearling
+                    />
+                        </div>
+                    </ReviewSliderSty>
                 </div>
-                <button className= 'submit' type = 'submit'>SUBMIT</button>
+                <button className= 'submit' type = 'submit'>Confirm</button>
             </form>
             </AddReviewSty>
         </div>
