fdc0b62266a3b2c44273b9c66a749e49ebeced8c
Cam Chandler <camchandler15@gmail.com>
2021-08-20 10:26:49 -0400

    added route to add friend

--- backend/routes/api/friends.js
+++ backend/routes/api/friends.js
@@ -12,6 +12,24 @@ router.get('/:userId', asyncHandler( async (req,res) => {
     let friends = await Friend.getUserFriends(userId)
     res.json(friends)
 
-} ))
+}))
+
+router.post('/', asyncHandler( async (req,res)=> {
+    let {userOneId,userTwoId} = req.body
+    let friendCheck = await Friend.findAll({
+        where: {
+            userOneId:{[Op.or]: [userOneId,userTwoId]},
+            userTwoId: {[Op.or] : [userOneId,userTwoId]}
+        }
+    })
+    if(friendCheck.length >= 1){
+        res.json('already friends')
+        return
+    }
+
+     let newRecord = await Friend.create({userOneId,userTwoId})
+
+     res.json(newRecord)
+}))
 
 module.exports = router
